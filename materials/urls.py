f75256c35940f887600f1ca437cc2140a81486d6
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-02-11 05:38:27 +1000

    done task 3

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView ,LessonUpdateAPIView, LessonDestroyAPIView
 from materials.apps import MaterialsConfig
 
 
@@ -11,5 +11,9 @@ router = SimpleRouter()
 router.register('', CourseViewSet)
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_detail'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 ] + router.urls
