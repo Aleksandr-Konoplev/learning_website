06fddfc3d90a0d8ff2b13206d2222e054ad113cc
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-02-12 21:41:50 +1000

    just a commit

--- materials/urls.py
+++ materials/urls.py
@@ -14,16 +14,16 @@ from materials.apps import MaterialsConfig
 app_name = MaterialsConfig.name
 
 router = SimpleRouter()
-router.register("", CourseViewSet)
+router.register('', CourseViewSet)
 
 urlpatterns = [
-    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
-    path("lessons/", LessonListAPIView.as_view(), name="lessons_list"),
-    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_detail'),
     path(
-        "lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"
+        'lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'
     ),
     path(
-        "lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"
+        'lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'
     ),
 ] + router.urls
