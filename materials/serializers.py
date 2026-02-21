3ccadaf34916032e3ba5fdf576a30bc9581c140a
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-02-21 18:24:44 +1000

    add subscrubles serialaizer

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,6 +3,8 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from materials.models import Course, Lesson
 from materials.validators import WebLinkValidator
 
+from users.models import Subscription
+
 
 class LessonSerializer(ModelSerializer):
     class Meta:
@@ -16,6 +18,7 @@ class CourseSerializer(ModelSerializer):
     """Сериалайзер курсов"""
     count_lesson_in_course = SerializerMethodField()
     lessons_info = SerializerMethodField()
+    is_subscribed = SerializerMethodField()
 
     @staticmethod
     def get_count_lesson_in_course(course):
@@ -26,6 +29,18 @@ class CourseSerializer(ModelSerializer):
         lessons = Lesson.objects.filter(course=course.pk)
         return LessonSerializer(lessons, many=True).data
 
+    def get_is_subscribed(self, course):
+        request = self.context.get('request')
+
+        # если пользователь не авторизован
+        if not request or not request.user.is_authenticated:
+            return False
+
+        return Subscription.objects.filter(
+            user=request.user,
+            course=course
+        ).exists()
+
     class Meta:
         model = Course
-        fields = ('name', 'count_lesson_in_course', 'lessons_info', 'description')
+        fields = ('name', 'count_lesson_in_course', 'lessons_info', 'description', 'is_subscribed')
